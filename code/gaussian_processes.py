0914059f64323571d1ea710a2b01b798fa3f0a20
maltesie <malte.siemers@mailbox.org>
2017-12-06 20:03:31 +0100

    gaussion processes fixed

--- code/gaussian_processes.py
+++ code/gaussian_processes.py
@@ -19,11 +19,10 @@ policy = CategoricalMLPPolicy(
 
 x = []
 y = []
-for i in range(20):
+for i in range(10):
     observation = env.reset()
     done = False
     while not done:
-        print(observation)
         action, probs = policy.get_action(observation)
         x.append([action, *observation])
         
@@ -32,25 +31,29 @@ for i in range(20):
 
 x = np.array(x)
 x -= x.mean(axis=0)
-
+#print(x)
 y = np.array(y)
-print(x.shape, len(y))
 
 # Instanciate a Gaussian Process model
-kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+kernel = RBF(10, (1e-2, 1e2))
 gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
 
-gp.fit(x, y.reshape(-1,1))
+gp.fit(x, y.reshape(-1,4))
 y_pred, sigma = gp.predict(x, return_std=True)
 
+x = x[:,1]
 fig = plt.figure()
+fig.clf()
 plt.plot(x, y, 'r.', markersize=10, label=u'Observations')
 plt.plot(x, y_pred, 'b-', label=u'Prediction')
+"""
 plt.fill(np.concatenate([x, x[::-1]]),
          np.concatenate([y_pred - 1.9600 * sigma,
                         (y_pred + 1.9600 * sigma)[::-1]]),
          alpha=.5, fc='b', ec='None', label='95% confidence interval')
 plt.xlabel('$x$')
 plt.ylabel('$f(x)$')
-plt.ylim(-10, 20)
+"""
+plt.ylim(-3, 3)
 plt.legend(loc='upper left')
+plt.show()
